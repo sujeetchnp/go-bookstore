681f6bd2c2c50dea2edf0ab2ab117932c9e68a78
sujeetchnp <sujeetchnp@gmail.com>
2023-03-09 23:24:02 -0500

    Fixed typo

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -10,7 +10,7 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "sujeet:Admin@123@/simplerest?charset=utf8&parseTime=TYrue&loc=Local")
+	d, err := gorm.Open("mysql", "sujeet:Admin@123@/simplerest?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}

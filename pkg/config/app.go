648e4320d4eb3fe98d8bfc7a79bb614f3100622b
sujeetchnp <sujeetchnp@gmail.com>
2023-03-10 19:21:34 -0500

    Changes to postgres connection

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,8 +1,10 @@
 package config
 
 import (
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
+	// "github.com/jinzhu/gorm"     //mysql
+	// _ "github.com/jinzhu/gorm/dialects/mysql"   //mysql
+	"gorm.io/driver/postgres" // postgres
+	"gorm.io/gorm"            //postgres
 )
 
 var (
@@ -10,7 +12,11 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "root:Admin@123*#()$@/bookinfo?charset=utf8&parseTime=True&loc=Local")
+	// d, err := gorm.Open("mysql", "root:Admin@123*#()$@/bookinfo?charset=utf8&parseTime=True&loc=Local")    // mysql
+
+	dsn := "host=localhost user=postgres password=Admin@123*#()$ dbname=bookinfo port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	d, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
 	if err != nil {
 		panic(err)
 	}
